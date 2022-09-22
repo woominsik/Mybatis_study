cdb269e29cbde0dac3845377ceb1dc7f58eeed06
woominsik <9737638@naver.com>
2022-09-22 11:23:19 +0900

    세팅

---
+++ src/main/java/com/ll/exam/Mybatis_practice/MybatisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.Mybatis_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/Mybatis_practice/MybatisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.Mybatis_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
